1039b5998a981942e124a5a79f34e2595e956c2c
mukeshr-29 <mukeshr2911@gmail.com>
2023-08-16 13:37:56 +0530

    empty pipeline

--- bin/cdk-pipeline-eb-demo.ts
+++ bin/cdk-pipeline-eb-demo.ts
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkPipelineEbDemoStack } from '../lib/cdk-pipeline-eb-demo-stack';
+import { CdkPipelineStack } from '../lib/cdk-pipeline-stack';
 
 const app = new cdk.App();
-new CdkPipelineEbDemoStack(app, 'CdkPipelineEbDemoStack', {
+
+new CdkPipelineStack(app, 'CdkPipelineStack', {
+   env: { account: '966624725593', region: 'us-east-1' },
+});
+
+app.synth();
+
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -18,4 +24,3 @@ new CdkPipelineEbDemoStack(app, 'CdkPipelineEbDemoStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
